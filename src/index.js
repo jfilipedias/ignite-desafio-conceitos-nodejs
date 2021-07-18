dbda8c1620b11b47b6a9074d9755cb4a7600a821
Filipe Dias <filipediascontato@gmail.com>
2021-07-18 17:10:27 -0300

    Implements todos patch resource

--- src/index.js
+++ src/index.js
@@ -66,7 +66,6 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   const { user } = request;
   const { title, deadline } = request.body;
   const { id } =  request.params;
-
   const todo = user.todos.find(todo => todo.id === id);
 
   if (!todo)
@@ -79,7 +78,16 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo)
+    return response.status(404).json({ error: "Todo not found." });
+  
+  todo.done = true;
+
+  return response.status(201).send();
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
