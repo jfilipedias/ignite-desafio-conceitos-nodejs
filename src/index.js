b3f8bbd256e31f024cc86775eeb8f6d132ca1644
Filipe Dias <filipediascontato@gmail.com>
2021-07-18 13:39:57 -0300

    Implements todos put resource

--- src/index.js
+++ src/index.js
@@ -62,8 +62,20 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
   return response.status(201).json(todo);
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => { 
-  // Complete aqui
+app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { title, deadline } = request.body;
+  const { id } =  request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo)
+    return response.status(404).json({ error: 'Todo not found.'});
+  
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.status(201).send();  
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
